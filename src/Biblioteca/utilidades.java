7bca72c4fe20f2aa4c87e590be33d0308a43dc01
Pablo Quesada <paablo.quesada@gmail.com>
2024-12-14 10:00:13 +0100

    prueba

---
+++ src/Biblioteca/utilidades.java
@@ -0,0 +1,7 @@
+package Biblioteca;
+
+public class utilidades {
+    public static void main(String[] args) {
+        System.out.println("Hola esto es una prueba");
+    }
+}
---
+++ src/fernanStarterT03.java
@@ -0,0 +1,5 @@
+public class fernanStarterT03 {
+    public static void main(String[] args) {
+        System.out.println("Hola esto es una prueba");
+    }
+}
