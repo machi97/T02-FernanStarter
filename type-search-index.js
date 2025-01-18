ba4c017a12643d55994468300c8849a3d61e870c
Pablo Quesada <paablo.quesada@gmail.com>
2025-01-18 16:21:56 +0100

    Terminadooo

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"biblioteca","l":"utilidades"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"utilidades","l":"funcionesCadenas"}];updateSearchResults();
